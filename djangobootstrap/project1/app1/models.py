ed42e906175a0c66536933534e6650d8862fce35
Edgar-Sumabat-Jr <165621324+Edgar-Sumabat-Jr@users.noreply.github.com>
2024-09-22 07:18:33 -0700

    Add files via upload

--- djangobootstrap/project1/app1/models.py
+++ djangobootstrap/project1/app1/models.py
@@ -1,6 +1,3 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    # Add any additional fields you want to store for each user
+# Create your models here.
